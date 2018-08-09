e4789dfa4f4107162bc3bb029330c1becae43924
firyalmahakhan <firyalmahak@gmail.com>
2018-08-09 20:42:49 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -8,7 +8,7 @@ weather = {
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
 
-def location(@nyc_weather)
+def location(nyc_weather)
 	puts "New York"["66.3"]
 end
 
