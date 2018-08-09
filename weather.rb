6d93be97c4c5f1d187f3116176305bfe5bf32632
firyalmahakhan <firyalmahak@gmail.com>
2018-08-09 20:54:03 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -16,7 +16,8 @@ end
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
 def description
-  puts description["Partly Cloudy"]
+  return weather ["Partly Cloudy"]
+  description
 end
 
 
